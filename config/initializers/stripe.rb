b49a87016fc5af6cf0a4c22bfed9742128964ae3
Keith_hoka <keith_d_hau@hotmail.com>
2021-10-28 20:39:41 +1100

    version_3.0

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_51JnFoXFPWQ7X0DS9OqekhyT3fo3Fj3ATaZlLHk9xLM6aakj8WPlKt7AIf1sLxqexnYAL8fZ4ppVSa2dxUJcjoWMm00xasskY2f'],
-  
-  :secret_key      => ENV['sk_test_51JnFoXFPWQ7X0DS9RmmCQJf5XNlzUdvEBQNoGhKhMUyGIcVcnnHQAOLNtoMnVxB1kQBodi2GlXKhFNy94RmjcDA30018ry4BoA']
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
